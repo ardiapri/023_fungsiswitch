708f782d0544761da313ef33c9b09e60a02d44b1
ardiapri <ardiapri882@gmail.com>
2024-03-26 17:35:01 +0700

    Add project files.

---
+++ prakpemdas.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+    cout << "masukan bilangan pertama = ";
+    cin >> bilA;
+    cout << "masukan bilangan kedua = ";
+    cin >> bilB;
+
+}
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+
+}
